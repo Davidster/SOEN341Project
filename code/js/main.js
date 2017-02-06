41f3d932a9f03e1c6707dd643c1e15927a2e1f60
David Huculak <davidhuculak5@gmail.com>
2017-02-06 17:37:16 -0500

    generalized page navigation system; converted pure js code to JQuery; fixed some issues in css

--- code/js/main.js
+++ code/js/main.js
@@ -1,15 +1,19 @@
-console.log("Hello World");
+$(document).ready(function(){
 
-function displayAccountCreation(e){
-    if (e.onclick)
-        document.getElementById('createAccount').style.display = 'block';
-    else
-      document.getElementById('createAccount').style.display = 'none';
-    }
-function displayAccountLogin(e){
-    if (e.onclick)
-        document.getElementById('login').style.display = 'block';
-    else
-        document.getElementById('login').style.display = 'none';
-    }	
-	
+    // create click event listener for all <li> tags inside the <ul class="menu" id="menu">
+    // notice how were using css selectors to grab objects from the html
+    $(".menu li").click(function(e){
+
+        var pageTarget = $(this).attr("pagetarget");
+        console.log("pageTarget: " + pageTarget);
+
+        // make all pages display-none
+        $("#page-content > div").css("display", "none");
+
+        // make the page were targeting display-block
+        // here we are building a css selector
+        $("#" + pageTarget).css("display", "block");
+
+    });
+
+});
