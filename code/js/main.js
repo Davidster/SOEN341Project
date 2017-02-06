efed068033fe3a346a6f562958fd3df92963bc5b
“MikelShifrin” <miki.super@gmail.com>
2017-02-05 19:54:52 -0500

    added favicon, added basic structure to index.html, added style to index.css and some basic functions to javascript file

--- code/js/main.js
+++ code/js/main.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+function displayAccountCreation(e){
+    if (e.onclick)
+        document.getElementById('createAccount').style.display = 'block';
+    else
+      document.getElementById('createAccount').style.display = 'none';
+    }
+function displayAccountLogin(e){
+    if (e.onclick)
+        document.getElementById('login').style.display = 'block';
+    else
+        document.getElementById('login').style.display = 'none';
+    }	
+	
