84dfbfc948f3b7ceb048f25a61b1e9a7a3e4146a
MikelShifrin <miki.super@gmail.com>
2017-03-08 11:09:19 -0500

    Update coursePage.php

--- code/php/index/coursePage.php
+++ code/php/index/coursePage.php
@@ -1,17 +1,13 @@
 <div id="course-page" style="display: none;">
-			
-			
-			
 	<h2>Courses Page</h2>
 	<p>Select the class corresponding to your group project. You will be redirected to the course page.</p>
-
 	<div class="dropdown" >
-				  <button class="btn">Courses</button>
-				  <div class="content" style="left:0;">
-						<a href="#">SOEN 341</a>
-						<a href="#">SOEN 490</a>
-						<a href="#">SOEN 287</a>
-						<a href="#">COEN 390</a>
-				  </div>
-				</div>			
-			</div>
+		<button class="btn">Courses</button>
+		<div class="content" style="left:0;">
+			<a href="#">SOEN 341</a>
+			<a href="#">SOEN 490</a>
+			<a href="#">SOEN 287</a>
+			<a href="#">COEN 390</a>
+		</div>
+	</div>			
+</div>
