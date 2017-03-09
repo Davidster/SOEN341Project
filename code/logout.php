1def2f63d204b93fbcd151b1a607be0010d94ee3
“MikelShifrin” <miki.super@gmail.com>
2017-03-08 21:12:53 -0500

    created personalised page upon login with php session

--- code/logout.php
+++ code/logout.php
@@ -1,5 +1,5 @@
 <?php   
 session_start(); //to ensure you are using same session
 session_destroy(); //destroy the session
-header("../../index.php"); //to redirect back to "index.php" after logging out
+header("location: index.php"); //to redirect back to "index.php" after logging out
 ?>
