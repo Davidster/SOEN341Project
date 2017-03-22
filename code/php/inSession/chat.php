22322ced09738b2f506c9d23344c752108432432
David Huculak <davidhuculak5@gmail.com>
2017-03-21 23:03:58 -0400

    added css file for styling phpfreechat windows

--- code/php/inSession/chat.php
+++ code/php/inSession/chat.php
@@ -44,6 +44,7 @@
 		<title>personal page</title>
 		<meta charset="UTF-8" />
 		<link rel="stylesheet" type="text/css" href="../../css/index.css"/>
+		<link rel="stylesheet" type="text/css" href="../../css/chat.css"/>
 
 		<!-- Import JQuery library (REMOVE THIS COMMENT AT SOME POINT) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
