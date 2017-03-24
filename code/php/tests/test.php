8a8c9dc407aea238ca5df1397671dfaf87f15b08
David Huculak <davidhuculak5@gmail.com>
2017-03-24 01:36:35 -0400

    added a test get request

--- code/php/tests/test.php
+++ code/php/tests/test.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace code\php\tests;
+require './vendor/autoload.php';
+use Guzzle\Http\Client;
 	
 /**
  * This file contains the unit tests for our project
@@ -9,8 +10,33 @@ namespace code\php\tests;
  */
 final class Test extends \PHPUnit_Framework_TestCase
 {
+
 	public function testDBCExists(){
 		require_once './code/sql_connect.php';
 		$this->assertEquals(isset($dbc), true);
+	}
+
+	public function testGET(){
+
+		//require './vendor/autoload.php'
+
+		// Create a client and provide a base URL
+		$client = new Client('http://localhost:80');
+
+		$request = $client->get('/code/php/index/home.php');
+		echo $request->getUrl();
+		// >>> https://api.github.com/user
+
+		// You must send a request in order for the transfer to occur
+		$response = $request->send();
+
+		echo $response->getBody();
+		// >>> {"type":"User", ...
+
+		echo $response->getHeader('Content-Length');
+		// >>> 792
+
+		// >>> User
+		$this->assertEquals(true, true);
 	}
 }
