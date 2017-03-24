f195f3ddd7bec8f9943823adb6008c23f53a482c
David Huculak <davidhuculak5@gmail.com>
2017-03-24 00:33:25 -0400

    added unit test which checks that  exists after requiring sql_connect.php

--- code/php/tests/test.php
+++ code/php/tests/test.php
@@ -3,14 +3,14 @@
 namespace code\php\tests;
 	
 /**
+ * This file contains the unit tests for our project
+ * 
  * @covers All
  */
 final class Test extends \PHPUnit_Framework_TestCase
 {
-	public function testTrueEqualsTrue()
-	{
-		$this->assertEquals(true, true);
+	public function testDBCExists(){
+		require_once './code/sql_connect.php';
+		$this->assertEquals(isset($dbc), true);
 	}
 }
-
-	 
