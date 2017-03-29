354464049f71ceeed6fafd283cac5515ad109732
Megatron4537 <nicolas_horta_adam3@hotmail.com>
2017-03-29 12:57:29 -0400

    Revert "Minor Changes on design of the upload Button on viewGroup Page, userstory #7 and changed animsition on login and create account pages and resolved all the conflicts"

--- code/php/index/login.php
+++ code/php/index/login.php
@@ -101,7 +101,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-	<div class="animsitionLogin" >
+	<div class="animsition1" >
 
 		<nav class="navbar navbar-inverse">
 			<div class="container-fluid">
