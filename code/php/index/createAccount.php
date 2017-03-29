3a6daa46c85a5cb62b67c2655452ee7264bb56a3
Mikel Shifrin <miki.super@gmail.com>
2017-03-28 20:47:01 -0400

    made createAccount.php more error proof

--- code/php/index/createAccount.php
+++ code/php/index/createAccount.php
@@ -37,66 +37,123 @@
 			</div>
 		</nav>
 		<div class="container">
-            <h1>Register Page</h1>
-				<form id='createAccount' action='' method='post'>
-                    <div class="form-group">
-				        <label><p>Enter your Concordia ID:</p></label>
-				            <input type='text' name='id' class="form-control" placeholder='Concordia ID' required/>
-                    </div>
-                    <div class="form-group">
-						<label><p>Enter your full name:</p></label>
-				        <input type='text' name='fullName' class="form-control" placeholder='Full Name' required/>
-                    </div>
-                    <div class="form-group">
-						<label><p>Enter your email:</p></label>
-						<input type='email' name='email' class="form-control" placeholder='Email Address' required/>
-                    </div>
-                    
-                    <div class="form-group">
-						<label><p>Enter your class and section:</p></label>
-						<input type='text' name='c1' class="form-control" placeholder='Class' required/>
-						<input type='text' name='s1' class="form-control" placeholder='Section' required/>
-                    </div>
-                    
-                    <div class="form-group">
-						<label><p>Enter your class and section:</p></label>
-						<input type='text' name='c2' class="form-control" placeholder='Class' />
-                    	<input type='text' name='s2' class="form-control" placeholder='Section' />
-                    </div>
-                    
-                    <div class="form-group">
-						<label><p>Enter your class and section:</p></label>
-						<input type='text' name='c3' class="form-control" placeholder='Class' />
-                   		<input type='text' name='s3' class="form-control" placeholder='Section' />
-                    </div>
-
-                    <div class="form-group">
-						<label><p>Enter your class and section:</p></label>
-						<input type='text' name='c4' class="form-control" placeholder='Class' />
-                    	<input type='text' name='s4' class="form-control" placeholder='Section' />
-                    </div>
-                    
-                    <div class="form-group">
-						<label><p>Enter your class and section:</p></label>
-						<input type='text' name='c5' class="form-control" placeholder='Class' />
-                   		<input type='text' name='s5' class="form-control" placeholder='Section' />
-                    </div>
-
+			<h1 class='col-xs-4'>Register Page</h1>
+			<form id='createAccount' action='' method='post'>
+				<div class='form-group col-xs-12'>
+					<label><p>Enter your Concordia ID:</p></label>
+					<input type='text' name='id' class='form-control' placeholder='Concordia ID' required/>
+				</div>
+				<div class="form-group col-xs-12">
+					<label><p>Enter your full name:</p></label>
+					<input type='text' name='fullName' class="form-control" placeholder='Full Name' required/>
+				</div>
+				<div class="form-group col-xs-12">
+					<label><p>Enter your email:</p></label>
+					<input type='email' name='email' class="form-control" placeholder='Email Address' required/>
+				</div>
+				<div class='form-group col-xs-12'>
+					<label for="sel1"><p>Select number of classes you're taking:</p></label>
+					<select class="form-control" id="sel1" name="sel1"  onchange="hello()" required>
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+					</select>
+					<br>
+				</div>
+				<div id = 'container'>
+				</div>
+				<div class="form-group col-xs-12">
+					<label><p>Create a Password:</p></label>
+					<input type='password' name='password' class="form-control" placeholder='Password' required/>
+				</div>
+				<div class="form-group col-xs-12">
+					<label><p>Confirm Password:</p></label>
+					<input type='password' name='repassword' class="form-control" placeholder='Password' required/>
+				</div>
+				<br/>
+				<div class="form-group col-xs-12">
+					<p><button type="submit" value = 'Submit' name='register' class="btn btn-default">Submit</button></p>
+				</div>
+			</form><br/><br/>
+		</div>  
+		<script>
+			document.onload(hello());
+			function hello(){
+				var number = document.getElementById("sel1").value;
             
-                    
-                    <div class="form-group">
-						<label><p>Create a Password:</p></label>
-						<input type='password' name='password' class="form-control" placeholder='Password' required/>
-                    </div>
-                    <div class="form-group">
-						<label><p>Confirm Password:</p></label>
-						<input type='password' name='repassword' class="form-control" placeholder='Password' required/>
-                    </div>
-                    <br>
+				// Container <div> where dynamic content will be placed
+				var container = document.getElementById("container");
+				//var stuff = document.createElement("input");
+				//container.appendChild(stuff);
+				while (container.hasChildNodes()) {
+					container.removeChild(container.lastChild);
+				}
+				for (i=0;i<number;i++){
+					// Create an <input> element, set its type and name attributes
+					var div = document.createElement("div");
+					div.className = "form-group col-xs-12";
+					var label = document.createElement('label');
+					var p = document.createElement('p');
+					var text = document.createTextNode("Enter your class and section:");
+					p.appendChild(text);
+					label.appendChild(p);
+					div.appendChild(label);
+               
+					//creating Class dropdown selection
+					var number1 = i + 1; 
+					var selectClass = document.createElement('select');
+					selectClass.className = "form-control";
+					selectClass.placeholder = "Class";
+					selectClass.name = 'c' + number1;
+				
+					var classText1 = document.createTextNode("SOEN341");
+					var classText2 = document.createTextNode("ENGR201");
+				
+					var optionClass1 = document.createElement("option");
+					var optionClass2 = document.createElement("option");
+					
+					//optionClass1.disabled = true;
+					optionClass1.appendChild(classText1);
+					optionClass2.appendChild(classText2);
+					
+					selectClass.appendChild(optionClass1);
+					selectClass.appendChild(optionClass2);
 
-                    <button type="submit" value = 'Submit' name='register' class="btn btn-default">Submit</button>
-					</form><br><br>
-        </div>
+					//creating section dropdown selection	
+					var selectSection = document.createElement('select');
+					selectSection.className = "form-control";
+					selectSection.placeholder = "Section";
+					selectSection.name = 's' + number1;
+
+					var sectionText1 = document.createTextNode("AA");
+					var sectionText2 = document.createTextNode("BB");
+					var sectionText3 = document.createTextNode("XX");
+					var sectionText4 = document.createTextNode("YY");
+				
+					var optionSection1 = document.createElement("option");
+					var optionSection2 = document.createElement("option");
+					var optionSection3 = document.createElement("option");
+					var optionSection4 = document.createElement("option");
+				
+					//option1.disabled = true;
+					optionSection1.appendChild(sectionText1);
+					optionSection2.appendChild(sectionText2);
+					optionSection3.appendChild(sectionText3);
+					optionSection4.appendChild(sectionText4);
+				
+					selectSection.appendChild(optionSection1);
+					selectSection.appendChild(optionSection2);
+					selectSection.appendChild(optionSection3);
+					selectSection.appendChild(optionSection4);                
+				
+					div.appendChild(selectClass);
+					div.appendChild(selectSection);
+					container.appendChild(div);
+                }
+			}
+		</script>
 			  <?php 
 				//gets user input
 				if(isset($_POST['register'])){
@@ -134,7 +191,7 @@
 							//create student record
 							if(mysqli_query($dbc, $registerQuery)){
 								//look through all his classes input
-								for($i = 1; $i <= 5; $i++){
+								for($i = 1; $i <= $_POST["sel1"]; $i++){
 									$s = "s$i";
 									$c = "c$i";
 									if(isset($_POST[$c]) && isset($_POST[$s])){
