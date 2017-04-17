5ade8f79568440f3d8e3ac532f90f6a358436acb
David Huculak <davidhuculak5@gmail.com>
2017-04-16 22:50:52 -0400

    added use of php command as static analysis

--- code/build.sh
+++ code/build.sh
@@ -1,3 +1,11 @@
-php -l ./code/index.php
 php -l ./code/sql_connect.php
-phpunit ./code/php/tests/test.php
+php -l ./code/index.php
+php -l ./code/php/index/about.php
+php -l ./code/php/index/createAccount.php
+php -l ./code/php/index/home.php
+php -l ./code/php/index/login.php
+php -l ./code/php/inSession/chat.php
+php -l ./code/php/inSession/logOut.php
+php -l ./code/php/inSession/myProfile.php
+php -l ./code/php/inSession/viewGroup.php
+phpunit ./code/php/tests/unitTests.php
